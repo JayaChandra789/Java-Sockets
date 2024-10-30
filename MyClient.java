40703fe023b5d5624c45a369e8739acdcb029576
JayaChandra789 <bagamjaya789@gmail.com>
2024-10-30 14:36:03 +0530

    MyClient.java

--- MyClient.java
+++ MyClient.java
@@ -1,5 +1,22 @@
 package newProjects;
 
-public class MyClient {
-
+public class MyClient 
+{
+public static void main(String[] args) 
+	{
+		try
+		{
+			Socket s=new Socket ("localhost", 6666);
+			DataOutputStream dout=new
+					DataOutputStream(s.getOutputStream());
+			dout.writeUTF("HelloServer");
+			dout.flush();
+			dout.close();
+			s.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
 }
